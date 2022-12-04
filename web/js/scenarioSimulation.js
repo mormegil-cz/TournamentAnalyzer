efaf501f92ae947c8994dfd2aec9e49f5cf8693e
Mormegil <mormegil@centrum.cz>
2022-12-04 22:01:48 +0100

    Update

--- web/js/scenarioSimulation.js
+++ web/js/scenarioSimulation.js
@@ -713,7 +713,7 @@
         ARG: 2125, // 2118, // 2101, // 2086, // 2141,
         ESP: 2007, // 2056, // 2068, // 2045,
         NED: 2068, // 2047, // 2036, // 2050, // 2040,
-        FRA: 1993, // 2046, // 2022, // 2005,
+        FRA: 2018, // 1993, // 2046, // 2022, // 2005,
         BEL: 1948, // 1948, // 2020, // 2007,
         POR: 1993, // 2044, // 2010, // 2006,
         DEN: 1883, // 1928, // 1952, // 1971,
@@ -721,12 +721,12 @@
         URU: 1905, // 1890, // 1924, // 1936,
         SUI: 1928, // 1901, // 1911, // 1902,
         CRO: 1945, // 1945, // 1914, // 1922,
-        ENG: 1969, // 1944, // 1957, // 1920,
+        ENG: 1995, // 1969, // 1944, // 1957, // 1920,
         SRB: 1835, // 1862, // 1882, // 1898,
         ECU: 1842, // 1885, // 1871, // 1840,
         MEX: 1813, // 1794, // 1809, // 1809,
         IRN: 1779, // 1809, // 1760, // 1817,
-        POL: 1827, // 1844, // 1814, // 1814,
+        POL: 1802, // 1827, // 1844, // 1814, // 1814,
         USA: 1819, // 1840, // 1810, // 1797, // 1798,
         JPN: 1841, // 1792, // 1831, // 1798,
         WAL: 1717, // 1742, // 1791, // 1790,
@@ -736,7 +736,7 @@
         CRC: 1737, // 1762, // 1723, // 1743,
         AUS: 1772, // 1779, // 1734, // 1702, // 1719,
         TUN: 1747, // 1694, // 1726, // 1687,
-        SEN: 1773, // 1730, // 1677, // 1687,
+        SEN: 1747, // 1773, // 1730, // 1677, // 1687,
         QAT: 1578, // 1589, // 1642, // 1680,
         KSA: 1643, // 1662, // 1692, // 1640,
         CMR: 1679, // 1621, // 1601, // 1610,
@@ -797,7 +797,7 @@
             new Group('F', ["BEL", "CAN", "MAR", "CRO"], preparePresetMatches({ 'MAR-CRO': '0:0', 'BEL-CAN': '1:0', 'BEL-MAR': '0:2', 'CRO-CAN': '4:1', 'CRO-BEL': '0:0', 'CAN-MAR': '1:2' }), RULES.FIFA),
             new Group('G', ["BRA", "SRB", "SUI", "CMR"], preparePresetMatches({ 'SUI-CMR': '1:0', 'BRA-SRB': '2:0', 'CMR-SRB': '3:3', 'BRA-SUI': '1:0', 'SRB-SUI': '2:3', 'CMR-BRA': '1:0' }), RULES.FIFA),
             new Group('H', ["POR", "GHA", "URU", "KOR"], preparePresetMatches({ 'URU-KOR': '0:0', 'POR-GHA': '3:2', 'KOR-GHA': '2:3', 'POR-URU': '2:0', 'GHA-URU': '0:2', 'KOR-POR': '2:1' }), RULES.FIFA),
-            new PlayoffTree('_result', ['A#1', 'B#2', 'C#1', 'D#2', 'E#1', 'F#2', 'G#1', 'H#2', 'B#1', 'A#2', 'D#1', 'C#2', 'F#1', 'E#2', 'H#1', 'G#2'], RULES.FIFA, preparePresetMatches({ 'NED-USA': '3:1', 'ARG-AUS': '2:1' }))
+            new PlayoffTree('_result', ['A#1', 'B#2', 'C#1', 'D#2', 'E#1', 'F#2', 'G#1', 'H#2', 'B#1', 'A#2', 'D#1', 'C#2', 'F#1', 'E#2', 'H#1', 'G#2'], RULES.FIFA, preparePresetMatches({ 'NED-USA': '3:1', 'ARG-AUS': '2:1', 'FRA-POL': '3:1', 'ENG-SEN': '3:0' }))
         ];
 
         validateScenario(scenario, rating);
@@ -849,7 +849,7 @@
                 interestingResults.push(scenarioResults.full);
             }
 
-            let sfTeams = results.stageTeams[results.stageTeams.length - 2].slice();
+            let sfTeams = results.stageTeams[results.stageTeams.length - 3].slice();
             sfTeams.sort();
             var id = sfTeams.join('+');
             let currCount = phaseTeamCounts[id] || 0;
